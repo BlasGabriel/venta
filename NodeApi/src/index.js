f7fb5ba3e734ad61bc82c7640e86d2794343bde5
Gabriel <blasgabriel200@gmail.com>
2024-03-10 15:33:24 -0300

    a

--- NodeApi/src/index.js
+++ NodeApi/src/index.js
@@ -1,13 +1,33 @@
 import express from "express";
 import ciudadesRoutes from "./routes/ciudad.routes.js";
-// import categoryRoutes from "./routes/categories.routes.js";
+import os from "os";
+import chalk from "chalk";
 
+const port = 4000;
+const appUrl = `/api`;
+const ipAddress = getIpAddress();
+const appUrlIP = `http://${ipAddress}:${port}`;
+const appUrllocalhost = `http://localhost:${port}`;
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/ciudades", ciudadesRoutes);
-// app.use("/api", categoryRoutes);
+app.use(`${appUrl}/ciudades`, ciudadesRoutes);
 
-app.listen(3000);
-console.log("Server on port", 3000);
+app.listen(port);
+console.log(`Server running at ${chalk.magenta(appUrllocalhost)} and ${chalk.magenta(appUrlIP)}`);
+
+function getIpAddress() {
+    const ifaces = os.networkInterfaces();
+    let ipAddress = "localhost";
+
+    Object.keys(ifaces).forEach((ifname) => {
+        ifaces[ifname].forEach((iface) => {
+            if (iface.family === "IPv4" && !iface.internal) {
+                ipAddress = iface.address;
+            }
+        });
+    });
+
+    return ipAddress;
+}
