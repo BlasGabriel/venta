217f748b99b4aa691369e474740fd8745c4aa295
Gustavo <gustavoortigoza06@gmail.com>
2024-04-04 22:11:52 -0400

    Se han agregado los controladores y las rutas para Clientes

--- NodeApi/src/routes/Routes.js
+++ NodeApi/src/routes/Routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import ciudadesRoutes from "./ciudad.routes.js";
 import usuarioRoutes from "./usuario.routes.js";
 import productoRoutes from "./producto.routes.js";
+import clienteRoutes from "./cliente.routes.js";
 
 const router = express.Router();
 
 router.use("/ciudades", ciudadesRoutes);
 router.use("/usuario", usuarioRoutes);
 router.use("/producto", productoRoutes);
+router.use("/clientes", clienteRoutes);
 
 export default router;
