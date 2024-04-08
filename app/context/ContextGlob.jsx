3d19d6d64e7c824f8336a1f5245382e5377835d3
Gabriel <blasgabriel200@gmail.com>
2024-04-07 20:55:17 -0400

    ProductosProvider

--- app/context/ContextGlob.jsx
+++ app/context/ContextGlob.jsx
@@ -2,23 +2,26 @@
 
 import { createContext } from "react";
 import { UserProvider } from "./UserContext";
+import { ProductosProvider } from "./ProductosContext";
 
 export const ContextGlob = createContext();
 
 export const use = () => {
-    const context = use(ContextGlob);
-    if (!context) {
-      throw new Error("use must be used within a ContextProvider");
-    }
-    return context;
-  };
-  
+  const context = use(ContextGlob);
+  if (!context) {
+    throw new Error("use must be used within a ContextProvider");
+  }
+  return context;
+};
+
 
 export const ContextGlobProvider = ({ children }) => {
   return (
-   
-      <UserProvider>
+
+    <UserProvider>
+      <ProductosProvider>
         {children}
-      </UserProvider>
+      </ProductosProvider>
+    </UserProvider>
   );
 };
