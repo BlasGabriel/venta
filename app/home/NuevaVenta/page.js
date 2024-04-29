c1025500e167d257fa68790ada7f13a61799054f
Gabriel <blasgabriel200@gmail.com>
2024-04-28 21:03:46 -0400

    a

--- app/home/NuevaVenta/page.js
+++ app/home/NuevaVenta/page.js
@@ -1,12 +1,25 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import { useProductos } from '@/app/context/ProductosContext';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, Stack, TablePagination, InputAdornment } from '@mui/material';
-import ButtonDE from '@/app/components/ButtonDE';
-import { useClientes } from '@/app/context/ClientesContext';
-import BoxTable from '@/app/components/containers/BoxTable';
+import React, { useEffect, useState } from "react";
+import { useProductos } from "@/app/context/ProductosContext";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TextField,
+  Stack,
+  TablePagination,
+  InputAdornment,
+  Box,
+} from "@mui/material";
+import ButtonDE from "@/app/components/ButtonDE";
+import { useClientes } from "@/app/context/ClientesContext";
+import BoxTable from "@/app/components/containers/BoxTable";
 import SearchIcon from "@mui/icons-material/Search";
-import Carrito from './Carrito';
+import Carrito from "./Carrito";
 
 function Page() {
   const { products, getProducts } = useProductos();
@@ -15,27 +28,40 @@ function Page() {
   const { getClientes, getClienteRuc, getClienteId } = useClientes();
 
   function addToCart(product) {
-    // Obtener la cantidad del producto o establecerla como 1 si no se proporciona
-    const qty = quantity[product.id_producto] || 1;
-    console.log({ ...product, quantity: qty });
-    setCart([...cart, { ...product, quantity: qty }]);
+    // Verificar si el producto ya está en el carrito
+    const existingProductIndex = cart.findIndex(
+      (item) => item.id_producto == product.id_producto
+    );
+
+    if (existingProductIndex !== -1) {
+      // Si el producto ya está en el carrito, actualizar su cantidad
+      const updatedCart = [...cart];
+      updatedCart[existingProductIndex].quantity++;
+      setCart(updatedCart);
+    } else {
+      // Si el producto no está en el carrito, agregarlo con cantidad 1
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  }
+
+  function removeFromCart(productId) {
+    // Filtrar el carrito para excluir el producto que se va a quitar
+    const updatedCart = cart.filter((item) => item.id_producto !== productId);
+    setCart(updatedCart);
   }
 
   useEffect(() => {
     const fetchData = async () => {
-        await getProducts();
-        console.log(await getClientes());
-        console.log("RUC	", await getClienteRuc(1234567));
-        console.log("ID	", await getClienteId(7));
+      await getProducts();
+      console.log(await getClientes());
+      console.log("RUC	", await getClienteRuc(1234567));
+      console.log("ID	", await getClienteId(7));
     };
 
     fetchData();
-}, []);
+  }, []);
 
- 
-
-
-//Contro de cantidad de intem moviles
+  //Contro de cantidad de intem moviles
   //--------------------------------------------------------------------------
   // rows
   const [page, setPage] = useState(0);
@@ -56,15 +82,19 @@ function Page() {
     setPage(0);
   };
 
-  // filtros 
+  // filtros
   const filteredItem = products.filter((item) => {
     const searchTermLower = searchTerm.toLowerCase();
-    const descripcionIncludes = item.descripcion.toLowerCase().includes(searchTermLower);
-    const codigo_barraIncludes = item.codigo_barra.toLowerCase().includes(searchTermLower);
+    const descripcionIncludes = item.descripcion
+      .toLowerCase()
+      .includes(searchTermLower);
+    const codigo_barraIncludes = item.codigo_barra
+      .toLowerCase()
+      .includes(searchTermLower);
     // const apellidoIncludes = item.apellido.toLowerCase().includes(searchTermLower);
     // const nombreApellidoCombined = `${item.nombre} ${item.apellido}`.toLowerCase().includes(searchTermLower);
-  
-    return descripcionIncludes || codigo_barraIncludes ;
+
+    return descripcionIncludes || codigo_barraIncludes;
   });
   // --------------------------------------------------------------------------
   const paginatedItem = filteredItem.slice(
@@ -79,77 +109,108 @@ function Page() {
     <div>
       <h1>Productos Disponibles</h1>
       {/* <TableContainer component={Paper}> */}
+      <Box display="flex" flexDirection="row">
         <BoxTable>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Descripción</TableCell>
-              <TableCell align="right">Precio</TableCell>
-              <TableCell>Observación</TableCell>
-              <TableCell>Cantidad</TableCell> 
-              <TableCell><Carrito cart={cart} /></TableCell>
-            </TableRow>
-          </TableHead>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Descripción</TableCell>
+                <TableCell align="right">Precio</TableCell>
+                <TableCell>Observación</TableCell>
+                <TableCell>Cantidad</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {paginatedItem.map((product) => (
+                <TableRow key={product.id_producto}>
+                  <TableCell component="th" scope="row">
+                    {product.descripcion}
+                  </TableCell>
+                  <TableCell align="right">
+                    ${product.precio_venta_maximo}
+                  </TableCell>
+                  <TableCell>{product.observacion}</TableCell>
+                  <TableCell>
+                    <TextField
+                      type="number"
+                      value={quantity[product.id_producto] || ""}
+                      onChange={(e) =>
+                        setQuantity((prev) => ({
+                          ...prev,
+                          [product.id_producto]: parseInt(e.target.value) || 0,
+                        }))
+                      }
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <ButtonDE onClick={() => addToCart(product)}>
+                      Agregar al carrito
+                    </ButtonDE>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={filteredItem.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+          <Stack
+            // direction="row"
+            // justifyContent="center"
+            // alignItems="center"
+            sx={{ m: 2 }}
+            spacing={1}
+          >
+            <TextField
+              autoFocus
+              label="Buscar"
+              value={searchTerm}
+              onChange={handleSearchChange}
+              variant="outlined"
+              // sx={{ m: 2,  }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Stack>
+        </BoxTable>
+        <BoxTable>
+          <TableRow>
+            <TableCell>Descripción</TableCell>
+            <TableCell align="right">Precio</TableCell>
+            {/* <TableCell>Observación</TableCell> */}
+            {/* <TableCell>Cantidad</TableCell> */}
+            <TableCell>
+              <Carrito cart={cart} />
+            </TableCell>
+          </TableRow>
           <TableBody>
-            {paginatedItem.map((product) => (
-              <TableRow key={product.id_producto}>
-                <TableCell component="th" scope="row">
-                  {product.descripcion}
-                </TableCell>
-                <TableCell align="right">${product.precio_venta_maximo}</TableCell>
-                <TableCell>{product.observacion}</TableCell>
+            {cart.map((item) => (
+              <TableRow key={item.id_producto}>
                 <TableCell>
-                  <TextField
-                    type="number"
-                    value={quantity[product.id_producto] || ''}
-                    onChange={(e) =>
-                      setQuantity((prev) => ({
-                        ...prev,
-                        [product.id_producto]: parseInt(e.target.value) || 0,
-                      }))
-                    }
-                  />
+                  {item.descripcion} - Cantidad: {item.quantity}
                 </TableCell>
                 <TableCell>
-                  <ButtonDE onClick={() => addToCart(product)}>Agregar al carrito</ButtonDE>
+                  <ButtonDE onClick={() => removeFromCart(item.id_producto)}>
+                    Quitar
+                  </ButtonDE>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
-        </Table>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={filteredItem.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-        <Stack
-          // direction="row"
-          // justifyContent="center"
-          // alignItems="center"
-          sx={{ m: 2 }}
-          spacing={1}
-        >
-          <TextField
-            autoFocus
-            label="Buscar"
-            value={searchTerm}
-            onChange={handleSearchChange}
-            variant="outlined"
-            // sx={{ m: 2,  }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Stack>
         </BoxTable>
+      </Box>
       {/* </TableContainer> */}
     </div>
   );
