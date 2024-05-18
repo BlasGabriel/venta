5ac9e3eb33eb1665d666a9c585f3254243528772
Gabriel <blasgabriel200@gmail.com>
2024-05-17 21:18:13 -0400

    cobros

--- NodeApi/src/controllers/CobrosController.js
+++ NodeApi/src/controllers/CobrosController.js
@@ -1,144 +1,149 @@
-import {prisma} from '../db.js';
+import { prisma } from "../db.js";
 
-export const listar = async(req, res, next) => {
-    try{
-        const cobros = await prisma.cuenta_cobrar.findMany({
-            where: {
-                estado: 1,
-            },
-            include: {
-                cliente: true,
-                usuario: true
-            }
-        });
-        res.json(cobros);
-    }
-    catch(error){
-        next(error);
-    }
-}
+export const listar = async (req, res, next) => {
+  try {
+    const cobros = await prisma.cuenta_cobrar.findMany({
+      where: {
+        estado: 1,
+      },
+      include: {
+        cliente: true,
+        // usuario: true
+        item_cobro: true,
+      },
+    });
+    // Itera sobre cada cuenta por pagar
+    const cuentasConPagosCompletados = cobros.map((cuenta) => {
+      // Verifica si la cantidad de item_cobro coincide con la cantidad de item_cobro con es_cobrado=1
+      const pagosCompletados = cuenta.item_cobro.filter(
+        (pago) => pago.es_cobrado == 1
+      ).length;
+      // Asigna 1 si los pagos completados son iguales a la cantidad total de pagos, de lo contrario asigna 0
+      cuenta.pagos_completados = pagosCompletados == cuenta.item_cobro.length ? 1 : 0;
+      cuenta.por_pagar = cuenta.item_cobro.length - pagosCompletados;
+      cuenta.cuentas = cuenta.item_cobro.length;
+      return cuenta;
+    });
+    res.json(cuentasConPagosCompletados);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const buscar = async(req, res, next) => {
-    try{
-        const id = parseInt(req.params.id);
-        const cobro = await prisma.cuenta_cobrar.findFirst({
-            where: {
-                id_cuenta_cobrar: id,
-                estado: 1
-            },
-            include: {
-                cliente: true,
-                usuario: true
-            }
-        });
+export const buscar = async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const cobro = await prisma.cuenta_cobrar.findFirst({
+      where: {
+        id_cuenta_cobrar: id,
+        estado: 1,
+      },
+      include: {
+        cliente: true,
+        usuario: true,
+      },
+    });
 
-        if(cobro){
-            res.json(cobro);
-        }
-        else{
-            res.json('Registro de cobro no encontrado...');
-        }        
+    if (cobro) {
+      res.json(cobro);
+    } else {
+      res.json("Registro de cobro no encontrado...");
     }
-    catch(error){
-        next(error);
-    }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const insertar = async(req, res, next) => {
-    try{
-        const cobro = req.body;
+export const insertar = async (req, res, next) => {
+  try {
+    const cobro = req.body;
 
-        //se verifica si los datos del cliente corresponden a la venta registrada anteriormente
-        const venta = await prisma.venta.findFirst({
-            where: {
-                id_cliente: cobro.id_cliente
-            },
-            select: {
-                id_venta: true,
-                id_cliente: true
-            }
-        });
+    //se verifica si los datos del cliente corresponden a la venta registrada anteriormente
+    const venta = await prisma.venta.findFirst({
+      where: {
+        id_cliente: cobro.id_cliente,
+      },
+      select: {
+        id_venta: true,
+        id_cliente: true,
+      },
+    });
 
-        //si los datos de la venta se relacionan con el cliente dado, entonces se habilita la cuenta para cobrar por dicha venta
-        if(venta){
-            await prisma.cuenta_cobrar.create({
-                data: {
-                    id_venta: cobro.id_venta,
-                    id_cliente: cobro.id_cliente,
-                    fecha: new Date(cobro.fecha),
-                    fecha_pago: new Date(cobro.fecha_pago),
-                    monto: cobro.monto,
-                    porcentaje_interes: cobro.porcentaje_interes,
-                    tipo_interes: cobro.tipo_interes
-                }
-            });
-            res.json('Registro de cobro creado...');            
-        }
-        else{
-            //se emite el siguiente mensaje cuando la venta no corresponde al cliente seleccionado
-            res.json('Los datos del cliente no corresponden a la venta...');
-        }
+    //si los datos de la venta se relacionan con el cliente dado, entonces se habilita la cuenta para cobrar por dicha venta
+    if (venta) {
+      await prisma.cuenta_cobrar.create({
+        data: {
+          id_venta: cobro.id_venta,
+          id_cliente: cobro.id_cliente,
+          fecha: new Date(cobro.fecha),
+          fecha_pago: new Date(cobro.fecha_pago),
+          monto: cobro.monto,
+          porcentaje_interes: cobro.porcentaje_interes,
+          tipo_interes: cobro.tipo_interes,
+        },
+      });
+      res.json("Registro de cobro creado...");
+    } else {
+      //se emite el siguiente mensaje cuando la venta no corresponde al cliente seleccionado
+      res.json("Los datos del cliente no corresponden a la venta...");
     }
-    catch(error){
-        next(error);
-    }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const editar = async(req, res, next) => {
-    try{
-        const id = parseInt(req.params.id);
-        const cobro = req.body;
+export const editar = async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const cobro = req.body;
 
-        const venta = await prisma.venta.findFirst({
-            where: {
-                id_cliente: cobro.id_cliente
-            },
-            select: {
-                id_venta: true,
-                id_cliente: true
-            }
-        });
+    const venta = await prisma.venta.findFirst({
+      where: {
+        id_cliente: cobro.id_cliente,
+      },
+      select: {
+        id_venta: true,
+        id_cliente: true,
+      },
+    });
 
-        if(venta){
-            await prisma.cuenta_cobrar.update({
-                where: {
-                    id_cuenta_cobrar: id
-                },
-                data: {
-                    id_venta: cobro.id_venta,
-                    id_cliente: cobro.id_cliente,
-                    fecha: new Date(cobro.fecha),
-                    fecha_pago: new Date(cobro.fecha_pago),
-                    monto: cobro.monto,
-                    porcentaje_interes: cobro.porcentaje_interes,
-                    tipo_interes: cobro.tipo_interes
-                }
-            });
-            res.json('Registro de cobro editado...');
-        }
-        else{
-            res.json('Los datos del cliente no corresponden a la venta...');
-        }
+    if (venta) {
+      await prisma.cuenta_cobrar.update({
+        where: {
+          id_cuenta_cobrar: id,
+        },
+        data: {
+          id_venta: cobro.id_venta,
+          id_cliente: cobro.id_cliente,
+          fecha: new Date(cobro.fecha),
+          fecha_pago: new Date(cobro.fecha_pago),
+          monto: cobro.monto,
+          porcentaje_interes: cobro.porcentaje_interes,
+          tipo_interes: cobro.tipo_interes,
+        },
+      });
+      res.json("Registro de cobro editado...");
+    } else {
+      res.json("Los datos del cliente no corresponden a la venta...");
     }
-    catch(error){
-        next(error);
-    }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const eliminar = async(req, res, next) => {
-    try{
-        const id = parseInt(req.params.id);
-        await prisma.cuenta_cobrar.update({
-            where: {
-                id_cuenta_cobrar: id
-            },
-            data: {
-                estado: 0
-            }
-        });
-        res.json('Registro de cobro eliminado...');
-    }
-    catch(error){
-        next(error);
-    }
-}
+export const eliminar = async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    await prisma.cuenta_cobrar.update({
+      where: {
+        id_cuenta_cobrar: id,
+      },
+      data: {
+        estado: 0,
+      },
+    });
+    res.json("Registro de cobro eliminado...");
+  } catch (error) {
+    next(error);
+  }
+};
